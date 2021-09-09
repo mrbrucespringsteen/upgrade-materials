fab578f5519f5bf5a12ab4c3667d3727302fd857
Alex <alex.beciana@gmail.com>
2021-09-09 17:56:10 -0400

    Updated react-app package name and commands to build and start site

--- packages/react-app/build/service-worker.js
+++ packages/react-app/build/service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
-  "./precache-manifest.ae9c96d21898acc6988664d5dec3b60c.js"
-=======
-  "./precache-manifest.3dcad9f8b122ba72bd38cfa9df3d045a.js"
->>>>>>> ef406004d42d11a72e0313cab11f90d037326242
+  "./precache-manifest.83c02e816979c6dfb560ae86a40885ac.js"
 );
 
 self.addEventListener('message', (event) => {
