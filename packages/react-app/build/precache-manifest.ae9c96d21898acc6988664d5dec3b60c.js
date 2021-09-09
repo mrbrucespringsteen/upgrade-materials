e9a9c21f3b3c2071d981ceb3540a44a273faf542
Alex <alex.beciana@gmail.com>
2021-09-09 17:18:34 -0400

    Fixed /rules link to /faq in amulet finder

---
+++ packages/react-app/build/precache-manifest.ae9c96d21898acc6988664d5dec3b60c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4196aecedec1dbdee15d712340b2f2ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a6bb6687ec41cb3d626",
+    "url": "./static/css/2.a88f433c.chunk.css"
+  },
+  {
+    "revision": "8fc52af1e9afdcacd2ff",
+    "url": "./static/css/main.d0394705.chunk.css"
+  },
+  {
+    "revision": "4a6bb6687ec41cb3d626",
+    "url": "./static/js/2.ed86e30f.chunk.js"
+  },
+  {
+    "revision": "50f80eed06b0fae588e333309a50fc2c",
+    "url": "./static/js/2.ed86e30f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc52af1e9afdcacd2ff",
+    "url": "./static/js/main.e5e69bbf.chunk.js"
+  },
+  {
+    "revision": "257103dac285839da3f92c87abcf3ff5",
+    "url": "./static/js/main.e5e69bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7242a8842eb86339cc63",
+    "url": "./static/js/runtime-main.c8135626.js"
+  },
+  {
+    "revision": "7d7a237edbb955a3df5efa70d4c9faa0",
+    "url": "./static/media/CrimsonText-Bold.7d7a237e.woff"
+  },
+  {
+    "revision": "5752378ea0d7fd0f60dd0afd5729f568",
+    "url": "./static/media/CrimsonText-BoldItalic.5752378e.woff"
+  },
+  {
+    "revision": "9e8ea2e8536b947db4e17ba4629f8582",
+    "url": "./static/media/CrimsonText-Italic.9e8ea2e8.woff"
+  },
+  {
+    "revision": "ba34f058abfd052229271cb217e60fed",
+    "url": "./static/media/CrimsonText-Regular.ba34f058.woff"
+  },
+  {
+    "revision": "09e17ed5ca809259fbe963d3a2cdaf4d",
+    "url": "./static/media/CrimsonText-SemiBold.09e17ed5.woff"
+  },
+  {
+    "revision": "b8cc38a1f8a6ff509ab8fa3a6c270671",
+    "url": "./static/media/CrimsonText-SemiBoldItalic.b8cc38a1.woff"
+  },
+  {
+    "revision": "6353ee9674d5858daa2ad7e065e1ff11",
+    "url": "./static/media/YatraOne-Regular.6353ee96.ttf"
+  }
+]);
